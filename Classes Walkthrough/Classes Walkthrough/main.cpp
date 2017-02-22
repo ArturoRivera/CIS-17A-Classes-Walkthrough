6374fb7c523c5828c8fa78944d5ce5645e9d9fae
ncc <ncc>
2017-02-22 11:12:39 -0800

    Final Commit

--- Classes Walkthrough/Classes Walkthrough/main.cpp
+++ Classes Walkthrough/Classes Walkthrough/main.cpp
@@ -9,19 +9,33 @@ int main()
 	//Student art = Student("Arturo");
 	//cout << art.GetName() << endl;
 
-	
 	//getchar();
 
+	
+	
 	//Teacher james = Teacher("James");
 	//cout << james.GetName() << endl;
+	
 	//getchar();
+	
+	
+	
 	Teacher james = Teacher("james");
 	Course cis17 = Course("CIS17A", "Programming C++: Objects", james);
+	Student sam = Student("Samantha");
 	Student art = Student("Arturo");
 	Student bri = Student("Brianna");
+	Student jake = Student("Jake");
+	Student christian = Student("Christian");
 	
 	cis17.enrollStudent(art, 0);
-	cis17.enrollStudent(bri, 1);
+	cis17.enrollStudent(sam, 1);
+	cis17.enrollStudent(jake, 2);
+	cis17.enrollStudent(bri, 3);
+	cis17.enrollStudent(christian, 4);
+
+	cout << cis17.GetClassInfo() << endl;
+
 	getchar();
 	return 0;
 }
